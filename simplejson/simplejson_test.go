e76cd85f4daa78e79dcaad71d44646731f4692dc
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-28 13:56:20 -0800

    Add test for number out of range

--- simplejson/simplejson_test.go
+++ simplejson/simplejson_test.go
@@ -28,6 +28,22 @@ func TestSimpleJsonCmd(t *testing.T) {
 		}
 	})
 
+	t.Run("InvalidJSON - value NaN", func(t *testing.T) {
+		jsonData := `
+		{
+			"field1": "Hello",
+			"field2": NaN
+		}`
+
+		err := RunSimeleJsonWithJSONData(jsonData)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		} else {
+			fmt.Printf("Error: %v\n", err)
+		}
+	})
+
+
 	t.Run("InvalidJSON - number out of range", func(t *testing.T) {
 		jsonData := `
 		{
